2c53bb1cfb89099fe68225320bcbe2134c8a2107
Nigel Greenway <NigelGreenway@users.noreply.github.com>
2025-11-11 20:48:10 +0000

    fix: remove chars from branch name (#46)
    Changes:
     - Remove `+ ` from branch name when passing to the worktree add command
    
    Adds:
     - Test around bug
    
    closes: #41
    
    Co-authored-by: Nigel Greenway <nigel.greenway@futurepixels.co.uk>

--- src/interface/fzf_modules/commit_ops.sh
+++ src/interface/fzf_modules/commit_ops.sh
@@ -10,6 +10,6 @@ select_commit_with_fzf() {
       --print-query \
       --header="Enter: Select branch | $show_commit_ref_binding: switch to commit refs" \
       --bind="$show_commit_ref_binding:become(git --no-pager log --no-color --oneline --no-merges --decorate | fzf --print-query  --prompt='Select commit ref: ' | tail -1 | awk '{print \$1}')" |\
-    awk '{print $1}' |\
+    awk '{if (NF > 1) print $2; else print $1}' |\
     tail -1
 }
